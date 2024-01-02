35e7b93ae07e6f5b9bfe81252baeec532da2a56e
ArthurRFr <arthur_fronza2011@hotmail.com>
2024-01-02 20:08:14 -0300

    feat: hide multiple email addresses

--- src/contexts/global-context.tsx
+++ src/contexts/global-context.tsx
@@ -2,7 +2,19 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { ptBR } from '@clerk/localizations'
 
 const GlobalContextProvider: React.FC<any> = ({ children, session }) => {
-  return <ClerkProvider localization={ptBR}>{children}</ClerkProvider>
+  return (
+    <ClerkProvider
+      localization={ptBR}
+      appearance={{
+        elements: {
+          profileSection__emailAddresses: 'hidden',
+          profileSection__danger: 'hidden',
+        },
+      }}
+    >
+      {children}
+    </ClerkProvider>
+  )
 }
 
 export default GlobalContextProvider
